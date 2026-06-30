e99dbdf94d47e4379af05aab10097cb8346c90f9
OpticRish <raj15rishabh@outlook.com>
2026-06-30 20:17:39 +0530

    Update debug endpoint to check Node version and env

--- api/debug.js
+++ api/debug.js
@@ -1,4 +1,7 @@
 module.exports = async function handler(req, res) {
-  const envKeys = Object.keys(process.env);
-  return res.status(200).json({ keys: envKeys });
+  return res.status(200).json({
+    nodeVersion: process.version,
+    envKeys: Object.keys(process.env),
+    hasGeminiKey: !!process.env.GEMINI_API_KEY
+  });
 }
